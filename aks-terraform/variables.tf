3e8b2d553d7a58a88094cee8818527e2904214c6
Dzidulak <dzidulak@gmail.com>
2024-01-12 15:24:27 +0000

    Added aks cluster mian configuration files

---
+++ aks-terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "client_id" {
+  description = "the Azure Application ID, which identifies your application"
+  type = string
+  default = "value"
+}
+
+variable "client_secret" {
+  description = "Azure Application Secret, which serves as the application's password"
+  type = string
+  default = "value"
+}
+
+
+
+  
+  
